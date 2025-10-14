5738b0cdc2a0f703182343841d48cb19ba1f99b7
Marcos Motta <mrcsmotta1@gmail.com>
2025-10-14 16:29:42 -0300

    Fix to use vit in laravel default login view

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-export default {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
+module.exports = {
+    plugins: [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ],
 };
